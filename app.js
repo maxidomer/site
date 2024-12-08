0e61fd1769e450ead1449d927937366ec893abe9
maxidomer <telekiow@gmail.com>
2024-12-08 19:59:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //let newWin = window.open("about:blank", "hello", "width=500,height=900,left=530");
+button.addEventListener('click', async () => {
+  const perm = await Notification.reguestPermission()
+
+  if (perm === 'granted'){
+    const notification = new Notification('hi everyone', {
+      body: 'heyasdasdasdasdasdasd',
+      data: {
+        url: 'login.html'
+      },
+      icon: 'on_progress.png',
+      tag: 'notif',
+      action: {
+        url:'my_projects.html'
+      }
+    })
+  }
+})
